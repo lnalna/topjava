bd303e117e2f83b087aa4705411887eb41c63caf
lnalna <lnalna@yandex.ru>
2017-07-29 17:47:35 +0300

    HW03 Optional

--- src/test/java/ru/javawebinar/topjava/MealTestData.java
+++ src/test/java/ru/javawebinar/topjava/MealTestData.java
@@ -3,8 +3,45 @@ package ru.javawebinar.topjava;
 import ru.javawebinar.topjava.matcher.BeanMatcher;
 import ru.javawebinar.topjava.model.Meal;
 
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
+import static ru.javawebinar.topjava.model.BaseEntity.START_SEQ;
+
 public class MealTestData {
 
-    public static final BeanMatcher<Meal> MATCHER = new BeanMatcher<>();
+    public static final BeanMatcher<Meal> MATCHER = new BeanMatcher<>(
+            ((expected, actual) -> expected == actual ||
+                    (Objects.equals(expected.getId(),actual.getId())
+                            && Objects.equals(expected.getCalories(),actual.getCalories())
+                            && Objects.equals(expected.getDate(),actual.getDate())
+                            && Objects.equals(expected.getTime(),actual.getTime())
+                            && Objects.equals(expected.getDescription(),actual.getDescription())
+                    ))
+
+    );
+
+    public static final int MEAL1_ID = START_SEQ + 2;
+
+   // public static final Meal MEAL1 = new Meal(100002, LocalDateTime.of(2015, Month.MAY, 28, 10, 0), "Завтрак", 500);
+  //  public static final Meal MEAL2 = new Meal(100003, LocalDateTime.of(2015, Month.MAY, 28, 13, 0), "Обед", 1000);
+  //  public static final Meal MEAL3 = new Meal(100004, LocalDateTime.of(2015, Month.MAY, 28, 20, 0), "Ужин", 500);
+  //  public static final Meal MEAL4 = new Meal(100005, LocalDateTime.of(2015, Month.MAY, 29, 10, 0), "Завтрак", 500);
+  //  public static final Meal MEAL5 = new Meal(100006, LocalDateTime.of(2015, Month.MAY, 29, 13, 0), "Обед", 1000);
+  //  public static final Meal MEAL6 = new Meal(100007, LocalDateTime.of(2015, Month.MAY, 29, 20, 0), "Ужин", 510);
+    public static final Meal MEAL7 = new Meal(100008, LocalDateTime.of(2015, Month.MAY, 30, 10, 0), "Завтрак", 500);
+    public static final Meal MEAL8 = new Meal(100009, LocalDateTime.of(2015, Month.MAY, 30, 13, 0), "Обед", 1000);
+    public static final Meal MEAL9 = new Meal(100010, LocalDateTime.of(2015, Month.MAY, 30, 20, 0), "Ужин", 500);
+    public static final Meal MEAL10 = new Meal(100011, LocalDateTime.of(2015, Month.MAY, 31, 10, 0), "Завтрак", 500);
+    public static final Meal MEAL11 = new Meal(100012, LocalDateTime.of(2015, Month.MAY, 31, 13, 0), "Обед", 1000);
+    public static final Meal MEAL12 = new Meal(100013, LocalDateTime.of(2015, Month.MAY, 31, 20, 0), "Ужин", 510);
+
+    //public static final List<Meal> USER_MEALS = Arrays.asList(MEAL12,MEAL11,MEAL10,MEAL9,MEAL8,MEAL7,MEAL6, MEAL5, MEAL4, MEAL3, MEAL2, MEAL1);
+    public static final List<Meal> USER_MEALS = Arrays.asList(MEAL12, MEAL11, MEAL10, MEAL9, MEAL8, MEAL7);
+
+
 
 }
--- src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
@@ -2,15 +2,43 @@ package ru.javawebinar.topjava.service;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.bridge.SLF4JBridgeHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import ru.javawebinar.topjava.util.DbPopulator;
+
+import static ru.javawebinar.topjava.MealTestData.MATCHER;
+import static ru.javawebinar.topjava.MealTestData.USER_MEALS;
+import static ru.javawebinar.topjava.UserTestData.ADMIN_ID;
+
 
 /**
  * Created by Nikolay Lobachev .
  */
+
+@ContextConfiguration({
+                "classpath:spring/spring-app.xml",
+                "classpath:spring/spring-db.xml"
+                })
+@RunWith(SpringJUnit4ClassRunner.class)
 public class MealServiceTest {
+
+
+    static {
+        SLF4JBridgeHandler.install();
+    }
+
+    @Autowired
+    private MealService service;
+
+    @Autowired
+    private DbPopulator dbPopulator;
 
     @Before
     public void setUp() throws Exception {
-
+        dbPopulator.execute();
     }
 
     @Test
@@ -36,6 +64,7 @@ public class MealServiceTest {
     @Test
     public void testGetAll() throws Exception {
 
+        MATCHER.assertCollectionEquals(USER_MEALS,service.getAll(ADMIN_ID));
     }
 
     @Test
