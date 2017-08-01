02abe1523efea637c4fccded98ca635f79939887
lnalna <lnalna@yandex.ru>
2017-08-01 17:52:32 +0300

    HW03 Optional

--- src/main/java/ru/javawebinar/topjava/util/DbPopulator.java
+++ src/main/java/ru/javawebinar/topjava/util/DbPopulator.java
@@ -1,3 +1,4 @@
+/*
 package ru.javawebinar.topjava.util;
 
 import org.springframework.core.io.DefaultResourceLoader;
@@ -20,3 +21,4 @@ public class DbPopulator extends ResourceDatabasePopulator {
         execute(dataSource);
     }
 }
+*/
