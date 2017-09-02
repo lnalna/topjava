29fbaf5ea549a26cfeb6c18373a1821df71d69cc
lnalna <lnalna@yandex.ru>
2017-09-02 16:29:55 +0300

    1.1 RootControllerTest.testMeals

--- src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
@@ -1,15 +1,21 @@
 package ru.javawebinar.topjava.web;
 
 import org.junit.Test;
+import ru.javawebinar.topjava.AuthorizedUser;
 
 import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static ru.javawebinar.topjava.MealTestData.*;
 import static ru.javawebinar.topjava.UserTestData.USER;
 import static ru.javawebinar.topjava.model.AbstractBaseEntity.START_SEQ;
 
+
 public class RootControllerTest extends AbstractControllerTest {
+
+    public static final int USER_ID=100000;
+    public static final int ADMIN_ID=100001;
 
     @Test
     public void testUsers() throws Exception {
@@ -26,4 +32,41 @@ public class RootControllerTest extends AbstractControllerTest {
                         )
                 )));
     }
+
+    @Test
+    public void testUserMeals() throws Exception{
+        AuthorizedUser.setId(USER_ID);
+        mockMvc.perform(get("/meals"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("meals"))
+                .andExpect(forwardedUrl("/WEB-INF/jsp/meals.jsp"))
+                .andExpect(model().attribute("meals", hasSize(6)))
+                .andExpect(model().attribute("meals", hasItem(
+                        allOf(
+                                hasProperty("id", is(MEAL1_ID)),
+                                hasProperty("description", is(MEAL1.getDescription()))
+                        )
+                )));
+
+    }
+
+    @Test
+    public void testAdminMeals() throws Exception{
+        AuthorizedUser.setId(ADMIN_ID);
+        mockMvc.perform(get("/meals"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("meals"))
+                .andExpect(forwardedUrl("/WEB-INF/jsp/meals.jsp"))
+                .andExpect(model().attribute("meals", hasSize(2)))
+                .andExpect(model().attribute("meals", hasItem(
+                        allOf(
+                                hasProperty("id", is(ADMIN_MEAL_ID+1)),
+                                hasProperty("description", is(ADMIN_MEAL2.getDescription()))
+                        )
+                )));
+
+    }
+
 }
